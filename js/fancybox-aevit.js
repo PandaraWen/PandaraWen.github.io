a0b74d683471000a1bc2194b304befdf4be47592
PandaraWen <pandara_wen@foxmail.com>
2016-03-10 00:27:59 +0800

    Site updated: 2016-03-10 00:27:58

--- js/fancybox-aevit.js
+++ js/fancybox-aevit.js
@@ -1,6 +1,7 @@
 
 $(function() {
 	var winWidth = $(window).width();
+
 	$('.fancybox-aevit img').each(function(index, item) {
 		var imgSrc = $(item).attr('src');
 		var strIndex = imgSrc.indexOf('?imageView2');
@@ -9,7 +10,7 @@ $(function() {
 		if (strIndex && winWidth <= 678) {
 			imgSrc = imgSrc.substring(0, strIndex);
 			// console.log(imgSrc);
-			imgSrc = imgSrc + "?imageView2/1/w/" + winWidth + "/h/" + winWidth;
+			imgSrc = imgSrc + "?imageView2/2/w/" + winWidth;
 			// imgSrc = imgSrc + "?imageView2/1/w/" + Math.floor(winWidth * 1.2) + "/h/" + Math.floor(winWidth * 1.2);
 			$(item).attr('src', imgSrc);
 			// console.log(imgSrc);
